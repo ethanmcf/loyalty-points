67d46e4dd9839632bbb24ecb6c5617e2c01464d5
Veda Kesarwani <vedakesarwani@gmail.com>
2025-11-19 11:20:56 -0500

    fetch calls for promotions

---
+++ frontend/src/apis/promotionsApis.jsx
@@ -0,0 +1,94 @@
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+export async function getAllPromotions(authToken) {
+    const res = await fetch(`${baseURL}/promotions`, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json", 
+            Authorization: `Bearer ${authToken}`,
+        },
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        console.log(error.error);
+        throw new Error(error.Error);
+    }
+
+    return res.json();
+}
+
+export async function getPromotionById(authToken, promotionId) {
+    const res = await fetch(`${baseURL}/promotions/${promotionId}`, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json", 
+            Authorization: `Bearer ${authToken}`,
+        },
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        console.log(error.error);
+        throw new Error(error.Error);
+    }
+
+    return res.json();
+}
+
+export async function createPromotion(authToken, promotionData) {
+    const res = await fetch(`${baseURL}/promotions`, {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json", 
+            Authorization: `Bearer ${authToken}`,
+        },
+        body:JSON.stringify(promotionData),
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        console.log(error.error);
+        throw new Error(error.Error);
+    }
+
+    return res.json();
+}
+
+export async function updatePromotion(authToken, promotionId, promotionData) {
+    const res = await fetch(`${baseURL}/promotions/${promotionId}`, {
+        method: "PUT",
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`, 
+        },
+        body:JSON.stringify(promotionData),
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        console.log(error.error);
+        throw new Error(error.Error);
+    }
+
+    return res.json();
+}
+
+export async function deletePromotion(authToken, promotionId) {
+    const res = await fetch(`${baseURL}/promotions/${promotionId}`, {
+        method: "DELETE",
+        headers: { 
+            "Content-Type": "application/json", 
+            Authorization: `Bearer ${authToken}`,
+        },
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        console.log(error.error);
+        throw new Error(error.Error);
+    }
+
+    return;
+}
+
