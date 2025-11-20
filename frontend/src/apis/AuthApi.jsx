e112504eef9ae8c32cc8611b9b95c37063e7ecc0
ethanmcf <ethan_mcfarland@outlook.com>
2025-11-20 14:37:20 -0500

    Fixed backend issues from merge

--- frontend/src/apis/AuthApi.jsx
+++ frontend/src/apis/AuthApi.jsx
@@ -1,41 +1,43 @@
 const baseURL = `${import.meta.env.VITE_BACKEND_URL}/auth`;
 
 export async function login(utorid, password) {
-  console.log(utorid, password)
   const res = await fetch(`${baseURL}/tokens`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-   body: JSON.stringify ({
-        utorid,
-        password
-    })
+    body: JSON.stringify({
+      utorid,
+      password,
+    }),
   });
   if (!res.ok) {
     const err = await res.json();
     throw new Error(err.error);
   }
-  return await res.json();  // authToken, expiresAt
-
+  return await res.json(); // authToken, expiresAt
 }
 
 export async function resetPassword(utorid, newPassword, resetToken) {
-  console.log(utorid, newPassword, resetToken, `${baseURL}/tokens/${resetToken}`)
+  console.log(
+    utorid,
+    newPassword,
+    resetToken,
+    `${baseURL}/tokens/${resetToken}`
+  );
   const res = await fetch(`${baseURL}/resets/${resetToken}`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify ({
+    body: JSON.stringify({
       utorid,
-      password: newPassword
-    })
-    
+      password: newPassword,
+    }),
   });
   if (!res.ok) {
     const err = await res.json();
-    console.log(err.error)
+    console.log(err.error);
     throw new Error(err.error);
   }
 }
@@ -44,19 +46,16 @@ export async function createNewResetToken(utorid) {
   const res = await fetch(`${baseURL}/resets`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify ({
-      utorid
-    })
-    
+    body: JSON.stringify({
+      utorid,
+    }),
   });
 
   if (!res.ok) {
     const err = await res.json();
     throw new Error(err.error);
   }
-  return res.json();  // resetToken, expiresAt
+  return res.json(); // resetToken, expiresAt
 }
-
-
