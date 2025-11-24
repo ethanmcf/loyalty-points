d2768343a38dc3ce3a143e290eb64e0da9f02d62
Veda Kesarwani <vedakesarwani@gmail.com>
2025-11-24 01:27:45 -0500

    delete promos dialog

---
+++ frontend/src/components/delete-dialogs/DeletePromotionsDialog.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useUser } from "../../contexts/UserContext";
+import { getPromotionById, deletePromotion } from "../../apis/promotionsApi";
+
+/**
+ * Dialog component for deleting a promotion.
+ * It strictly mirrors the pattern of the DeleteUserDialog, 
+ * including pre-fetching details and using window.location.reload().
+ * @param {object} props - The component props.
+ * @param {number} props.id - The ID of the promotion to delete.
+ */
+export function DeletePromotionDialog({ id }) {
+  const { user } = useUser();
+  const [isOpen, setIsOpen] = useState(false);
+  const [deletedPromotion, setDeletedPromotion] = useState();
+
+  const fetchPromotion = async () => {
+    try {
+      const res = await getPromotionById(user.token, id);
+      setDeletedPromotion(res);
+    } catch (error) {
+      console.error(error);
+      setIsOpen(false); 
+    }
+  };
+
+  useEffect(() => {
+    fetchPromotion();
+  }, [id]);
+
+  const handleClickOpen = () => {
+    setIsOpen(true);
+  };
+  
+  const handleClose = () => {
+    setIsOpen(false);
+    // TODO based on state management
+    window.location.reload(); 
+  };
+
+  const handleDelete = async () => {
+    // adding the delete API call
+    try {
+        await deletePromotion(user.token, id);
+        handleClose();
+    } catch (error) {
+        console.error("Promotion deletion failed:", error);
+        //TODO need to show actual error msg not one console?
+    }
+  };
+
+  return (
+    <>
+      <Button variant="icon" onClick={handleClickOpen}>
+        <DeleteIcon color="error" />
+      </Button>
+      {deletedPromotion && (
+        <Dialog open={isOpen} onClose={handleClose}>
+          <DialogTitle>Delete Confirmation</DialogTitle>
+          <DialogContent>
+            Please confirm that you would like to delete the following promotion:
+            <p>
+              <b>Name:</b> {deletedPromotion.name}
+            </p>
+            <p>
+              <b>Type:</b> {deletedPromotion.type}
+            </p>
+            <p>
+              <b>Starts:</b> {new Date(deletedPromotion.startTime).toLocaleDateString()}
+            </p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button> 
+            <Button onClick={handleDelete} color="error" variant="contained"> 
+                Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </>
+  );
+} // may need to edit line 79/80 later 
