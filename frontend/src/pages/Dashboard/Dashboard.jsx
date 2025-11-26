018fd9a2e840323574de0552c5e76aff39a451c4
Nada Alomrani <nadaaalomrani@gmail.com>
2025-11-26 10:54:33 -0500

    Finished regular dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,19 @@
+import "./Dashboard.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useUser } from "../../contexts/UserContext";
+import { NavLink } from "react-router-dom";
+import RegularDashboard from "./RegularDashboard";
+import ManagerDashboard from "./ManagerDashboard";
+import CashierDashboard from "./CashierDashboard";
+
+// Table-related imports
+import { DataTable } from "../../components/data-table-mui/DataTable";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { AddUserDialog } from "../../components/addDialogs/AddUserDialog";
 
 
 export function Dashboard() {
@@ -8,24 +21,36 @@ export function Dashboard() {
 
   // get user info
   const { user } = useUser();
- 
+  const [roleView, setRoleView] = useState(user.role);
+
+  const handleRoleChange = (event) => {
+    setRoleView(event.target.value);
+  };
 
   return (
     <div>
-      <div>
-        <h1>Dashboard</h1>
-        <p>
-          Hi { user.name }!
-        </p>
-        <div>
-          place filters here
+      <div className="dashboard-container">
+        <div className="title-container">
+          <h2>Dashboard</h2>
+          <h3>
+            Hi { user.name }!
+          </h3>
         </div>
-        <div>
-          place table here
-        </div>
-        <div>
-          Points: { user.points }
-        </div>
+        {user.role === "superuser" && (
+          <ManagerDashboard />
+        )}
+
+        {user.role === "manager" && (
+          <ManagerDashboard />
+        )}
+
+        {user.role === "regular" && (
+          <RegularDashboard />
+        )}
+
+        {user.role === "cashier" && (
+          <CashierDashboard />
+        )}
       </div>
     </div>
   );
