bcedfc09a8ba823ab157f4932dd3d876128209bd
Veda Kesarwani <vedakesarwani@gmail.com>
2025-11-26 13:13:37 -0500

    edit

--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -1,5 +1,5 @@
 import { useUser } from "../../contexts/UserContext";
-import { DataTable } from "../../components/data-table-mui/DataTable";
+import { DataTable } from "../../components/data-table/DataTable";
 import { AddEventsDialog } from "../../components/addDialogs/AddEventsDialog"; 
 import "./Events.css"; 
 
