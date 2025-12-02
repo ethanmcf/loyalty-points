df250c7f7a3dc13c3b7fa5a6a821f97ee6703d6c
ethanmcf <ethan_mcfarland@outlook.com>
2025-12-02 13:52:21 -0500

    fixed muiappbr color

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      defaultProps: { elevation: 0, color: "transparent", position: "static" },
+      styleOverrides: {
+        root: {
+          backgroundColor: "#fff",
+        },
+      },
+    },
+  },
+});
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
     <App />
-  // </StrictMode>,
-)
+  </ThemeProvider>
+);
