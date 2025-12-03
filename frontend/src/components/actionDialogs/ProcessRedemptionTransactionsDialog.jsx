5789c5cf440c158745b9fce3faab5648618823ea
ethanmcf <ethan_mcfarland@outlook.com>
2025-12-02 19:08:13 -0500

    Update imorts

--- frontend/src/components/actionDialogs/ProcessRedemptionTransactionsDialog.jsx
+++ frontend/src/components/actionDialogs/ProcessRedemptionTransactionsDialog.jsx
@@ -10,7 +10,7 @@ import { useUser } from "../../contexts/UserContext";
 import {
   getTransaction,
   setTransactionCompleted,
-} from "../apis/transactionsApi";
+} from "../../apis/TransactionsApi";
 import Alert from "@mui/material/Alert";
 
 /**
