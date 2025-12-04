48432969aa5d92de4b88be462aa903f987701b40
jnnfrui <jenn.xinrui@gmail.com>
2025-12-03 20:02:39 -0500

    fix css issue with buttons

--- frontend/src/components/addDialogs/AddUserDialog.jsx
+++ frontend/src/components/addDialogs/AddUserDialog.jsx
@@ -10,11 +10,14 @@ import { useState } from "react";
 import Alert from "@mui/material/Alert";
 import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
+import { useUser } from "../../contexts/UserContext";
 
 export function AddUserDialog({ isOpen, setIsOpen }) {
+  const { user } = useUser();
   const [isCreated, setIsCreated] = useState(false);
   const [createdUser, setCreatedUser] = useState();
   const [error, setError] = useState();
+  const canAdd = user?.role === "manager" || user?.role === "superuser";
 
   const handleClickOpen = () => {
     setIsOpen(true);
@@ -49,16 +52,21 @@ export function AddUserDialog({ isOpen, setIsOpen }) {
 
   return (
     <>
-      <IconButton sx={{ display: { xs: "flex", md: "none" } }}>
-        <AddIcon onClick={handleClickOpen} />
-      </IconButton>
-      <Button
-        sx={{ display: { xs: "none", md: "flex" } }}
-        variant="text"
-        onClick={handleClickOpen}
-      >
-        Add New User
-      </Button>
+      {canAdd && (
+        <>
+          <IconButton sx={{ display: { xs: "flex", md: "none" } }}>
+            <AddIcon onClick={handleClickOpen} />
+          </IconButton>
+          <Button
+            sx={{ display: { xs: "none", md: "flex" } }}
+            variant="text"
+            onClick={handleClickOpen}
+          >
+            Add New User
+          </Button>
+        </>
+      )}
+
       <Dialog open={isOpen} onClose={handleClose}>
         {!isCreated ? (
           <>
